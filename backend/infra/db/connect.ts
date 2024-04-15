4c28b7d4b431e63b4227e6d38c1e25eb97ed648b
Alfredo Granja <josalfk@hotmail.com>
2024-04-15 11:41:24 -0600

    Add Seeding data

--- backend/infra/db/connect.ts
+++ backend/infra/db/connect.ts
@@ -1,12 +1,28 @@
 import mongoose from "mongoose";
+import { ContentTypeScheema, UserScheema } from "./scheemas";
 
 const host = process.env.MONGODB_HOST;
 const user = process.env.MONGODB_USER;
 const database = process.env.MONGODB_DATABASE;
 
-console.warn("Connection:",`${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}`)
+console.warn(
+  "Connection:",
+  `${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}`
+);
 async function connectDatabase() {
-  await mongoose.connect(`${host}/${database}`);
+  await mongoose.connect(`${host}/${database}`).then(() => {
+    UserScheema.create({
+      name: "Admin",
+      email: "admin@myapp.com",
+      type: "ADMIN",
+    });
+
+    ContentTypeScheema.create([
+      { description: "A video from youtube", name: "Youtube Video", type: "Video" },
+      { description: "A text post", name: "Text", type: "Text" },
+      { description: "An image post", name: "Image", type: "Image" },
+    ]);
+  });
 }
 
-export default connectDatabase
+export default connectDatabase;
